94e98fcfb4fad47c8b7c36de076c165569106b19
Belita Lynum <belita.lynum1@gmail.com>
2024-09-12 11:40:37 -0400

    Adding Mongoose, .env file and connected to database

--- vSchoolWork/schoolWork/ln5/fake-movies/app.js
+++ vSchoolWork/schoolWork/ln5/fake-movies/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 //Middleware (for every request)
 app.use(express.json()) //Looks for a request body, and turns it into 'req.body'
 app.use(morgan('dev'))
 
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+    }
+};
 
+connectToMongoDB();
 
 //Routes
 app.use("/movies", require("./routes/movieRouter"))
--- vSchoolWork/schoolWork/ln5/fake-movies/routes/movieRouter.js
+++ vSchoolWork/schoolWork/ln5/fake-movies/routes/movieRouter.js
@@ -22,12 +22,7 @@ movieRouter.post('/', (req, res) => {
     res.send(`Successfully added "${newMovie.title}" to the database`);
 }); */
 
-movieRouter.delete('/movies/:movieId', (req, res, next) => {
-    const movieId = req.params.movieId;
-    const movieIndex = movies.findIndex(movie => movie._id === movieId);
-    movies.splice(movieIndex, 1);
-    res.send('Successfully deleted movie!');
-});
+
 
 movieRouter.route("/")
     .get((req, res) => { // Remove '/' inside .get()
@@ -70,12 +65,40 @@ movieRouter.delete('/:movieID', (req, res) => {
     res.send('Successfully deleted movie!');
 })
 
+movieRouter.delete('/:movieId', async (req, res, next) => {
+    try {
+        const movieId = req.params.movieId;
+        const deletedMoive = await Movie.findByIdandUpdate(movieId)
+        return res.status(200).send('You have successfully deleted ${deletedMovie.title}')
+    } catch (error) {
+        res.status(500)
+        return next(err)
+    };
+});
+
+/* movieRouter.delete('/movies/:movieId', (req, res, next) => {
+    const movieId = req.params.movieId;
+    const movieIndex = movies.findIndex(movie => movie._id === movieId);
+    movies.splice(movieIndex, 1);
+    res.send('Successfully deleted movie!');
+}); */
+
 // Update One
-movieRouter.put("/:movieId", (req, res) => {
-    const movieId = req.params.movieId
-    const movieIndex = movies.findIndex(movie => movie._id === movieId)
-    const updatedMovie = Object.assign(movies[movieIndex], req.body)
-    res.send(updatedMovie)
+movieRouter.put("/:movieId", async (req, res, next) => {
+    try {
+        const movieId = req.params.movieId
+        const updatedMovie = await Movie.findByIdAndUpdate(
+            movieId,
+            req.body,
+            { new: true } //telling db we want the newest version back
+        )
+        return res.status(201).send(updatedMovie)
+    } catch (error) {
+        res.status(500)
+        return next(err)
+
+    }
+
 })
 
 module.exports = movieRouter
