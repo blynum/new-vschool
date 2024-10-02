d79890cc847e71c9da43414deba550653256171e
Belita Lynum <belita.lynum1@gmail.com>
2024-10-02 09:52:18 -0400

    Learning and creating authorization routes for signin, login, and user issues

--- vSchoolWork/schoolWork/lv6/rlm-token-auth-series-context/routes/authRouter.js
+++ vSchoolWork/schoolWork/lv6/rlm-token-auth-series-context/routes/authRouter.js
@@ -23,5 +23,27 @@ authRouter.post('/signup', async (req, res, next) => {
     }
 })
 
+authRouter.post('/login', async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        if (!user) {
+            res.status(403)
+            return next(new Error("Incorrect Username or Password"))
+        }
+        if (req.body.password !== user.password) {
+            res.status(403)
+            return next(new Error("Incorrect Username and Password"))
+        }
+        const token = jwt.sign(user.toObject(), process.env.SECRET)
+        return res.status(201).send({ user, token })
+    } catch (error) {
+        res.status(500)
+        return next(error)
+    }
+})
+
+
+
+
 
 module.exports = authRouter
